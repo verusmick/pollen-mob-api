69a6e40a04ccce388e29fe77f74d2a21cf1ab996
Miguel Angel <miguel.angel.angulo72@gmail.com>
2025-08-11 17:10:41 -0400

    Adding default mapge

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,20 @@ import { sendFeedback } from "../src/controllers/feedback.controller";
 const app = express();
 app.use(express.json());
 
-// rutas
+// Default page route (GET /)
+app.get("/", (req, res) => {
+  res.send(`
+    <html>
+      <head><title>Pollen API</title></head>
+      <body>
+        <h1>Welcome to Pollen API</h1>
+        <p>Try sending POST requests to <code>/send-feedback</code></p>
+      </body>
+    </html>
+  `);
+});
+
+// API route
 app.post("/send-feedback", sendFeedback);
 
 export default serverless(app);

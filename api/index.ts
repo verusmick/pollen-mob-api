d5b22e8c60ad4aa60d6b5b8c5a9807494836a698
Miguel Angel <miguel.angel.angulo72@gmail.com>
2025-08-13 09:54:24 -0400

    Testing server

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,5 @@
-import express from "express";
-import serverless from "serverless-http";
-import { sendFeedback } from "../src/controllers/feedback.controller";
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
-const app = express();
-app.use(express.json());
-
-// Root page for /api
-app.get("/", (req, res) => {
-  res.send(`
-    <html>
-      <head><title>Pollen API</title></head>
-      <body>
-        <h1>Welcome to Pollen API</h1>
-        <p>Try sending POST requests to <code>/send-feedback</code></p>
-      </body>
-    </html>
-  `);
-});
-
-// POST route for feedback
-app.post("/send-feedback", sendFeedback);
-
-export default serverless(app);
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).send('API is alive');
+}
